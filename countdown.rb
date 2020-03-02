3a9ba441634edd4c37af5cd622a2cb5b2bbe3e0a
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-02 15:21:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(sec)
   end
   'HAPPY NEW YEAR!'
 end
+
+def countdown_with_sleep(sec)
+  while sec > 0
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
+    sleep(1)
+  end
+  'HAPPY NEW YEAR!'
+end
